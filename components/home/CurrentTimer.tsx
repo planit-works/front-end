4a7ebfe75abfe939ae022745617875b47bb9e1a9
5thwin <hisy2059@naver.com>
2023-01-25 23:23:37 +0900

    feat: 시간 12,24시 표현 적용

--- components/home/CurrentTimer.tsx
+++ components/home/CurrentTimer.tsx
@@ -1,26 +1,49 @@
 import { useCallback, useEffect, useState } from 'react';
+import { getLocalStorage, setLocalStorage } from '../../utils/localStorage';
+import { getTimeByFormat } from '../../utils/time';
+
+type TimeDisplay = {
+  time: string;
+  format: 12 | 24;
+};
 export default function CurrentTimer() {
-  const [time, setTime] = useState<string>('00:00');
+  const [timer, setTimer] = useState<TimeDisplay>({
+    time: '00:00',
+    format: 24,
+  });
   const currentTimer = useCallback(() => {
-    const date = new Date();
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    setTime(`${hours}:${minutes}`);
-  }, []);
+    //로컬스토리지에서 timeformat을 가져와서 형식 변경
+    let newFormat: 12 | 24 = 24;
+    if (getLocalStorage('time-format') === '12') {
+      newFormat = 12;
+    }
+    const { hours, minutes } = getTimeByFormat(timer.format);
+    setTimer((pre) => ({ time: `${hours}:${minutes}`, format: newFormat }));
+  }, [timer.format]);
 
   useEffect(() => {
     currentTimer();
-    const timer = setInterval(() => currentTimer(), 1000);
+    const intervalId = setInterval(() => currentTimer(), 1000);
     //1초마다 currentTimer 반복
 
     return () => {
-      clearInterval(timer);
+      clearInterval(intervalId);
     };
   }, [currentTimer]);
 
   return (
-    <div>
-      <p className="text-white font-medium text-9xl">{time}</p>
+    <div className="group flex items-center justify-center">
+      <button className="w-10 invisible">1</button>
+      <span className="text-white font-medium text-9xl">{timer.time}</span>
+      <button
+        className="w-10 ml-1 invisible group-hover:visible text-white text-lg"
+        onClick={() => {
+          setLocalStorage('time-format', timer.format === 12 ? '24' : '12');
+          setTimer((pre) => ({ ...pre, format: pre.format === 12 ? 24 : 12 }));
+        }}
+      >
+        {`${timer.format}H`}
+      </button>
     </div>
   );
 }
