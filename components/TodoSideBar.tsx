e0f9b470bb799de8d59ed67d892afe4482d4a3b2
5thwin <hisy2059@naver.com>
2023-01-25 18:16:01 +0900

    feat: todo 사이드바 애니메이션

---
+++ components/TodoSideBar.tsx
@@ -0,0 +1,30 @@
+import { Dispatch, SetStateAction, useRef } from 'react';
+import { BsChevronRight } from 'react-icons/bs';
+
+interface TodoSideBarProps {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export default function TodoSideBar(props: TodoSideBarProps) {
+  const { isOpen, setIsOpen } = props;
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <div ref={containerRef} className="todo-side-bar animate-slipToLeft">
+      <p className="text-4xl font-bold">Todo</p>
+      <ul className="mx-3 my-2">
+        <li>할일</li>
+      </ul>
+      <button
+        className="absolute text-xl left-0 top-1/2 text-gray-400"
+        onClick={() => {
+          containerRef.current?.classList.add('animate-slipToRight');
+          setTimeout(() => setIsOpen(false), 500);
+        }}
+      >
+        <BsChevronRight />
+      </button>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,22 @@
 /* eslint-disable semi */
+import { useState } from 'react';
+import TodoSideBar from '../components/TodoSideBar';
+import { BsChevronLeft } from 'react-icons/bs';
 export default function Home() {
+  const [isOpenTodo, setIsOpenTodo] = useState<boolean>(false);
+
   return (
     <div className="w-screen flex flex-col items-center">
       <h1 className="text-sky-600 text-6xl font-bold mt-20">Hello world!</h1>
+      <button
+        className="absolute right-2 top-1/2 text-xl text-gray-400 flex items-center hover:animate-bounceLeft"
+        onClick={() => setIsOpenTodo(true)}
+      >
+        <BsChevronLeft /> <span>todo</span>
+      </button>
+      {isOpenTodo && (
+        <TodoSideBar isOpen={isOpenTodo} setIsOpen={setIsOpenTodo} />
+      )}
     </div>
   );
 }
