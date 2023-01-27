f1d2ca9af986e32e6ead40a452740652438fbe5b
HYK424 <turtle424@naver.com>
2023-01-27 21:32:10 +0900

    feat: tailwind custom animation 기능 추가

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -1,5 +1,7 @@
+import WelcomeQuestion from "@/components/welcome/welcomeQuestion";
+
 export default function welcome() {
   return ( <div className="w-screen flex flex-col items-center justify-center relative h-screen overflow-x-hidden bg-[url('../assets/background2.jpg')] bg-cover bg-top bg-center bg-fixed">
-    <div>9999999999999999999999</div>
+    <WelcomeQuestion welcomeText="안녕하세요. 우리 만난 적 있나요?"/>
   </div>);
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        intro: {
+          from: { opacity: '0' },
+          to: { opacity: '1' },
+        },
+      },
+      animation: {
+        intro: 'intro 2s ease-in-out',
+      },
+    },
   },
   plugins: [],
-}
+};
