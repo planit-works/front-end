8837dfe01484f22f2a54df355a4520671b2d1b06
HYK424 <turtle424@naver.com>
2023-01-27 20:20:35 +0900

    feat: 백그라운드 이미지 추가

---
+++ pages/welcome.tsx
@@ -0,0 +1,5 @@
+export default function welcome() {
+  return ( <div className="w-screen flex flex-col items-center justify-center relative h-screen overflow-x-hidden bg-[url('../assets/background2.jpg')] bg-cover bg-top bg-center bg-fixed">
+    <div>9999999999999999999999</div>
+  </div>);
+};
