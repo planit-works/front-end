2470bb18d7aadaea574221a523a84100b2876d8f
HYK424 <turtle424@naver.com>
2023-01-29 17:41:07 +0900

    feat: welcomeQuestion 문구 랜덤으로 렌더링

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -1,11 +1,17 @@
 import AnswerLink from '@/components/welcome/answerLink';
 import WelcomeAnswer from '@/components/welcome/welcomeAnswer';
 import WelcomeQuestion from '@/components/welcome/welcomeQuestion';
+import { welcomeTextArr } from '@/constants/welcomeTextArr';
+import { useEffect, useState } from 'react';
+import getRandomArr from './../utils/getRandomArr';
+import useWelcomeText from './../hooks/useWelcomeText';
 
 export default function welcome() {
+  const welcomeText = useWelcomeText();
+
   return (
     <div className="w-screen flex flex-col items-center justify-center relative h-screen overflow-x-hidden bg-[url('../assets/background2.jpg')] bg-cover bg-top bg-center bg-fixed">
-      <WelcomeQuestion welcomeText="안녕하세요. 우리 만난 적 있나요?" />
+      <WelcomeQuestion welcomeText={welcomeText} />
       <WelcomeAnswer />
     </div>
   );
