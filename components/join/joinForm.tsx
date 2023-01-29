26979e3c02e7ced0a05912ee6ac777c7e06f3c70
HYK424 <turtle424@naver.com>
2023-01-29 17:42:43 +0900

    feat: auth-join template 구현

---
+++ components/join/joinForm.tsx
@@ -0,0 +1,16 @@
+import { AuthInputText, AuthInputPsw } from '../authInput';
+
+export default function JoinForm() {
+  return (
+    <div className=" flex flex-col items-center justify-center w-1/2 relative bottom-[4rem] " >
+      JOIN
+      <form action="">
+        <AuthInputText placeHolder="Email을 입력해 주세요" />
+        <AuthInputPsw placeHolder="Password를 입력해 주세요" />
+        <AuthInputPsw placeHolder="Password를 다시 한 번 입력해 주세요" />
+      <button type="submit">회원가입</button>
+      <button type="submit"></button>
+      </form>
+    </div>
+  );
+}
