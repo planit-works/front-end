a9f47ca371c6de38b03a6c828657175cc608afcf
5thwin <hisy2059@naver.com>
2023-02-01 23:41:46 +0900

    feat: 체크박스로 todo done 수정

--- components/home/TodoItem.tsx
+++ components/home/TodoItem.tsx
@@ -1,36 +1,56 @@
 import { Todo } from 'types/Todo';
 import { BsFillTrashFill, BsPencilSquare } from 'react-icons/bs';
-import { useCallback } from 'react';
-import useTodoListStore from 'store/todoStore';
-import { setLocalStorage } from 'utils/localStorage';
-import { todoKey } from 'constants/keyValue';
+import { useTodo } from '../../hooks/useTodo';
+import React, { useState, useCallback, useRef } from 'react';
 
 interface TodoItemProps {
   todo: Todo;
 }
 
 export default function TodoItem({ todo }: TodoItemProps) {
-  const { todoList, setTodoList } = useTodoListStore();
-  const handleDelete = useCallback(() => {
-    const updatedList = todoList.filter((item) => item !== todo);
-    //localStorage에서 todo-list 업데이트
-    setLocalStorage(todoKey, updatedList);
-    //state에서 현재 todo 삭제
-    setTodoList(updatedList);
-  }, [setTodoList, todo, todoList]);
+  const { handleDelete, handleUpdate } = useTodo();
+  const [isUpdating, setIsUpdating] = useState<Boolean>(false);
+  const handleUpdateBtnClick = useCallback(() => {
+    setIsUpdating(true);
+  }, []);
+  const handleTitleUpdate = useCallback(
+    (event: React.FocusEvent<HTMLInputElement>) => {
+      console.log(event.currentTarget.value);
+      handleUpdate(todo.id, { ...todo, title: event.currentTarget.value });
+      setIsUpdating(false);
+    },
+    [handleUpdate, todo],
+  );
 
   return (
     <div className="flex w-full group items-center">
-      <input type="checkbox" name="done" defaultChecked={todo.done} />
-      <p className="ml-1">{todo.title}</p>
-      <div className="invisible group-hover:visible">
-        <button>
-          <BsPencilSquare />
-        </button>
-        <button onClick={handleDelete}>
-          <BsFillTrashFill />
-        </button>
-      </div>
+      <input
+        type="checkbox"
+        name="done"
+        defaultChecked={todo.done}
+        onChange={handleUpdate(todo.id, { ...todo, done: !todo.done })}
+      />
+      <p className="ml-1">
+        {isUpdating ? (
+          <input
+            defaultValue={todo.title}
+            onBlur={handleTitleUpdate}
+            autoFocus
+          />
+        ) : (
+          todo.title
+        )}
+      </p>
+      {!isUpdating && (
+        <div className="invisible group-hover:visible">
+          <button onClick={handleUpdateBtnClick}>
+            <BsPencilSquare />
+          </button>
+          <button onClick={handleDelete(todo.id)}>
+            <BsFillTrashFill />
+          </button>
+        </div>
+      )}
     </div>
   );
 }
