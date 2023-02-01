6b6b95fe3bf49f62c00c811e065dae591c383a70
5thwin <hisy2059@naver.com>
2023-02-02 00:06:47 +0900

    feat: todo 기능 추가 (#6)
    * feat: todo 사이드바 애니메이션
    
    * feat: 시간 출력
    
    * feat: 시간 12,24시 표현 적용
    
    * feat: todo 추가
    
    * feat: todo 삭제기능
    
    * feat: Chevron 버튼 컴포넌트 분리
    
    * feat: useTodo Hook 추가
    
    * feat: 체크박스로 todo done 수정

---
+++ components/ChevronButton.tsx
@@ -0,0 +1,34 @@
+import { MouseEventHandler } from 'react';
+import { BsChevronRight, BsChevronLeft } from 'react-icons/bs';
+
+interface ChevronButtonProps {
+  handleButtonClick: MouseEventHandler<HTMLButtonElement> | undefined;
+  classList?: string;
+}
+export function ChevronRightButton({
+  handleButtonClick,
+  classList,
+}: ChevronButtonProps) {
+  return (
+    <button
+      className={`absolute text-xl left-2 top-1/2 text-gray-400 ${classList}`}
+      onClick={handleButtonClick}
+    >
+      <BsChevronRight />
+    </button>
+  );
+}
+
+export function ChevronLeftButton({
+  handleButtonClick,
+  classList,
+}: ChevronButtonProps) {
+  return (
+    <button
+      className={`absolute text-xl left-2 top-1/2 text-gray-400 ${classList}`}
+      onClick={handleButtonClick}
+    >
+      <BsChevronLeft />
+    </button>
+  );
+}
--- utils/localStorage.ts
+++ utils/localStorage.ts
@@ -1,5 +1,9 @@
 export const setLocalStorage = (key: string, item: any) => {
-  localStorage.setItem(key, item);
+  if (typeof item === 'object') {
+    localStorage.setItem(key, JSON.stringify(item));
+  } else {
+    localStorage.setItem(key, item);
+  }
 };
 
 export const removeLocalStorage = (key: string) => {
