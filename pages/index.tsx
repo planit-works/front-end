df3a42eb3746d48ea6a27813f6fe0fabfc729267
5thwin <hisy2059@naver.com>
2023-02-27 00:25:11 +0900

    Revert "feat sidebar (#14)"
    This reverts commit 7c64d67ee60fbf8e3cbdf29843a18b7b9742771c.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable semi */
 import { useState } from 'react';
-import MainSideBar from '../components/home/MainSide/MainSideBar';
-
+import TodoSideBar from '../components/home/Todo/TodoSideBar';
 import { BsChevronLeft } from 'react-icons/bs';
 import Clock from '../components/home/Clock';
 import UserNavBar from 'components/UserNavBar';
@@ -9,7 +8,7 @@ export default function Home() {
   const [isOpenTodo, setIsOpenTodo] = useState<boolean>(false);
 
   return (
-    <div className="w-screen flex flex-col justify-center items-center relative h-screen overflow-x-hidden bg-[url('../assets/background2.jpg')] bg-cover bg-center">
+    <div className="w-screen flex flex-col justify-center items-center relative h-screen overflow-x-hidden bg-[url('../assets/background1.webp')] bg-cover">
       <div className="">
         <Clock />
       </div>
@@ -21,13 +20,8 @@ export default function Home() {
       </button>
       <UserNavBar />
       {isOpenTodo && (
-        <MainSideBar isOpen={isOpenTodo} setIsOpen={setIsOpenTodo} />
+        <TodoSideBar isOpen={isOpenTodo} setIsOpen={setIsOpenTodo} />
       )}
-      <img
-        src="/planit-small.png"
-        alt="planit"
-        className="absolute w-20 left-6 bottom-6 opacity-70"
-      />
     </div>
   );
 }
