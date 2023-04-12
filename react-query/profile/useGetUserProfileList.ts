8141fbffa9e59cefa11607cdbe4fb427b9171770
HYK424 <turtle424@naver.com>
2023-04-12 17:35:30 +0900

    refactor: react-query 로직 수정(회원가입, 프로필리스트)

--- react-query/profile/useGetUserProfileList.ts
+++ react-query/profile/useGetUserProfileList.ts
@@ -3,30 +3,32 @@ import { getUserProfileList } from 'api/profile/Api';
 import QueryKey from '../react-key';
 
 export const useGetUserProfileList = (id: string) => {
-  const { data, fetchNextPage, hasNextPage, isLoading } = useInfiniteQuery({
-    queryKey: [QueryKey.getUserProfileList, id],
-    queryFn: async ({ pageParam = 1 }) => getUserProfileList(id, pageParam),
-    getNextPageParam: (lastpage) => {
-      if (lastpage.paginationInfo.hasNextPage)
-        return lastpage.paginationInfo.currentPage + 1;
-      else return undefined;
-    },
-    enabled: id === '' ? false : true,
-    staleTime: 1000 * 60,
-    cacheTime: 1000 * 60 * 5,
-    retry: false,
-    onError: (error: Error) => {
-      console.log(error.message);
-    },
-    onSuccess: (data) => {
-      console.log(data.pages.map((data) => data.profiles));
-    },
-  });
+  const { data, fetchNextPage, hasNextPage, isLoading, refetch } =
+    useInfiniteQuery({
+      queryKey: [QueryKey.getUserProfileList, id],
+      queryFn: async ({ pageParam = 1 }) => getUserProfileList(id, pageParam),
+      getNextPageParam: (lastpage) => {
+        if (lastpage.paginationInfo.hasNextPage)
+          return lastpage.paginationInfo.currentPage + 1;
+        else return undefined;
+      },
+      enabled: id === '' ? false : true,
+      staleTime: 1000 * 60,
+      cacheTime: 1000 * 60 * 5,
+      retry: false,
+      onError: (error: Error) => {
+        console.log(error.message);
+      },
+      onSuccess: (data) => {
+        console.log(data.pages.map((data) => data.profiles));
+      },
+    });
 
   return {
     userProfileDatas: data?.pages,
     fetchNextPage,
     hasNextPage,
     isLoading,
+    refetch,
   };
 };
--- react-query/useJoinUser.ts
+++ react-query/useJoinUser.ts
@@ -1,13 +1,7 @@
-import {
-  UseMutateFunction,
-  useMutation,
-  useQueryClient,
-} from '@tanstack/react-query';
+import { UseMutateFunction, useMutation } from '@tanstack/react-query';
 import { createUser } from 'api/auth/Api';
 import useErrorStore from 'store/useErrorStore';
 import { AuthInfo } from 'types/auth';
-import QueryKey from './react-key';
-import Router from 'next/router';
 
 export const useJoinUser = (): UseMutateFunction<
   void,
@@ -15,7 +9,6 @@ export const useJoinUser = (): UseMutateFunction<
   AuthInfo,
   unknown
 > => {
-  const queryClient = useQueryClient();
   const { setError } = useErrorStore();
   const { mutate } = useMutation({
     mutationFn: (authInfo: AuthInfo) => createUser(authInfo),
