333f6e0bcd0c59c563777396b6afb1885b96811b
HYK424 <turtle424@naver.com>
2023-05-06 20:10:18 +0900

    style: 검색바 디자인 변경

--- components/search/SearchInput.tsx
+++ components/search/SearchInput.tsx
@@ -1,5 +1,5 @@
 import { AiOutlineSearch } from 'react-icons/ai';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import useDebounce from '../../hooks/useDebounce';
 import SearchedListBar from './SearchedList';
 import { useGetUserProfileList } from 'react-query/profile/useGetUserProfileList';
@@ -7,7 +7,7 @@ import { useGetUserProfileList } from 'react-query/profile/useGetUserProfileList
 export default function SearchInput() {
   const [searchVal, setSearchVal] = useState('');
   const debounceVal = useDebounce(searchVal);
-  const { userProfileDatas, fetchNextPage, hasNextPage, isLoading, refetch } =
+  const { userProfileDatas, fetchNextPage, hasNextPage } =
     useGetUserProfileList(debounceVal);
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -16,25 +16,21 @@ export default function SearchInput() {
 
   return (
     <div className="ml-4">
-      <form action="">
-        <div className="flex justify-center items-center w-[15rem]">
-          <input
-            onChange={handleSearch}
-            type="text"
-            className="w-[13rem] px-2 h-7 rounded-md focus:outline-none "
-            placeholder="5자 이상 입력해 주세요"
-          />
+      <div className="flex items-center w-[13rem] rounded-md bg-white">
+        <input
+          onChange={handleSearch}
+          type="text"
+          className="h-7 w-[10rem] focus:outline-none bg-transparent ml-2"
+          placeholder="5자 이상 입력해 주세요"
+        />
 
-          <button type="button">
-            <AiOutlineSearch className="h-6 w-[2rem] text-zinc-300" />
-          </button>
-          <SearchedListBar
-            userProfileDatas={userProfileDatas}
-            fetchNextPage={fetchNextPage}
-            hasNextPage={hasNextPage}
-          />
-        </div>
-      </form>
+        <AiOutlineSearch className="relative left-1 m-[0px] h-6 w-[2rem] text-zinc-300" />
+        <SearchedListBar
+          userProfileDatas={userProfileDatas}
+          fetchNextPage={fetchNextPage}
+          hasNextPage={hasNextPage}
+        />
+      </div>
     </div>
   );
 }
