464a7667d22a510d1d337242d1124ef667673562
HYK424 <turtle424@naver.com>
2023-05-06 20:52:47 +0900

    style: update checker 반응형 적용 (md)

--- components/SliderUpdateChecker.tsx
+++ components/SliderUpdateChecker.tsx
@@ -7,10 +7,11 @@ export default function SliderUpdateChecker() {
   return (
     <div
       className={`${isUpdateCheckerSlider ? 'animate-sliderMove' : 'hidden'}
-      animate-sliderMove fixed bottom-[4rem] p-2 border-2 rounded-lg border-green-600 bg-green-600`}
+      animate-sliderMove fixed bottom-[4rem] w-[12rem] p-2 border-2 rounded-lg border-green-600 bg-green-600
+      md:w-[10.5rem]`}
     >
       <AiOutlineCheck className="inline mx-1" />
-      <span className="mx-2">Update Success!</span>
+      <span className="mx-2 md:text-sm">Update Success!</span>
     </div>
   );
 }
